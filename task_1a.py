8e8de8aaf4f9126648e397473857fc46507bfba0
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-10-03 13:46:14 +0530

    Update task_1a.py

--- task_1a.py
+++ task_1a.py
@@ -413,8 +413,8 @@ def detect_medicine_packages(maze_image):
 					# cv.drawContours(image, [i], -1, (0, 255, 0), 2)
 					l=[]
 					e=i+1
-					cx=cx
-					cy=cy
+					cx=e*100+cx
+					cy=100+cy
 					l.append(cx)
 					l.append(cy)
 					li.append(l)
@@ -444,8 +444,8 @@ def detect_medicine_packages(maze_image):
 					# cv.drawContours(image, [i], -1, (0, 255, 0), 2)
 					l=[]
 					t=i+1
-					cx=cx
-					cy=cy
+					cx=t*100+cx
+					cy=100+cy
 					l.append(cx)
 					l.append(cy)
 					li.append(l)
@@ -473,8 +473,8 @@ def detect_medicine_packages(maze_image):
 					# cv.drawContours(image, [i], -1, (0, 255, 0), 2)
 					l=[]
 					g=i+1
-					cx=cx
-					cy=cy
+					cx=g*100+cx
+					cy=100+cy
 					l.append(cx)
 					l.append(cy)
 					li.append(l)
